58bae6216d4c8aa24d4c5d4a527220c4b450e3e0
TAHRI Ahmed <nyuubi.10@gmail.com>
2016-01-11 18:51:21 +0100

    RbUnits: Nouvelle gestion des tests.

--- rbunits/l3s6.class.rb
+++ rbunits/l3s6.class.rb
@@ -1,6 +1,18 @@
-require 'ruby_units/namespaced'
+require 'test/unit'
+load 'class/l3s6.class.rb'
 
 #Vos tests dans ce fichier
 #https://github.com/olbrich/ruby-units
 
-puts "Début des tests pour class L3s6.class.rb .."
+
+class MyTest < Test::Unit::TestCase
+
+	def test_alpha
+		kDef = Def.new(199)
+		assert_equal(199, kDef.val)
+	end
+
+end
+
+#puts "Début des tests pour class L3s6.class.rb .."
+
