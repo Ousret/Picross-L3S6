847e1be3edddde0bc750cf4212f8fefff15de9eb
Baptiste <Baptiste.cv@gmail.com>
2016-01-19 14:56:58 +0100

    Adaptation des processus de cryptage à l'AES 128 GCM

--- class/crypt.class.rb
+++ class/crypt.class.rb
@@ -5,6 +5,7 @@ class Crypt
 	@state
 	@psw
 	@processedPsw
+	@iv
 	attr_reader :state
 	attr_reader :id
 
@@ -12,47 +13,46 @@ class Crypt
 		@state = "Newborn"
 		@psw=password
 		@processedPsw = self.determinate_key
+		@iv="NikeAdidasDiorPhilips "
 	end
 
 	def determinate_key
 		salt = "RoiDemonDu72"
 		iter = 17158
-		key_len = 12
+		key_len = 128
 		key = OpenSSL::PKCS5.pbkdf2_hmac_sha1(@psw, salt, iter, key_len)
 		return key
 	end
 
 	def encrypt(dataToEncrypt)
-		cipher = OpenSSL::Cipher::AES.new(128, :GCM)
+		cipher = OpenSSL::Cipher.new('aes-128-gcm')
 		cipher.encrypt
-		key = @processedPsw
-		iv = 666
+		cipher.key = @processedPsw
+		cipher.iv = @iv
 		if (ENV['USER'] != nil ) then
 			cipher.auth_data = ENV['USER']
 		else
 			cipher.auth_data = ""
 		end
-		
-		encrypted = cipher.update(dataToEncrypt) + cipher.final
-		tag = cipher.auth_tag
+		encrypted = cipher.update(dataToEncrypt)
+		#tag = cipher.auth_tag
 
 		return encrypted
 	end
 
 	def decrypt(dataToDecrypt)
-		decipher = OpenSSL::Cipher::AES.new(128, :GCM)
+		decipher = OpenSSL::Cipher.new('aes-128-gcm')
 		decipher.decrypt
 		decipher.key = @processedPsw
-		decipher.iv = 666
-		decipher.auth_tag = tag
+		decipher.iv = @iv
+		decipher.auth_tag = ENV['USER']
 		if (ENV['USER'] != nil ) then
 			decipher.auth_data = ENV['USER']
 		else
 			decipher.auth_data = ""
 		end
 
-		plain = decipher.update(dataToDecrypt) + decipher.final
+		plain = decipher.update(dataToDecrypt)
 		return plain
 	end
-
 end
