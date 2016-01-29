6ce2cd12dc13705794777b5690e282abe0a7c36f
Marius <maxetselina@gmail.com>
2016-01-29 09:36:00 +0100

    Update bmp.class.rb
    modifications d'un assert et des urls

--- rbunits/bmp.class.rb
+++ rbunits/bmp.class.rb
@@ -5,31 +5,31 @@ class MyTest < Test::Unit::TestCase
 
 	def test_bmp_new_bmp_1
 		2.times{
-			bmp = BMP::Reader.new("./bmp1.bmp")		
+			bmp = BMP::Reader.new("../ressources/bmp1.bmp")		
 		}
 	end
 
 	def test_bmp_new_bmp_2
 		2.times{
-			bmp = BMP::Reader.new("./bmp2.bmp")		
+			bmp = BMP::Reader.new("../ressources/bmp2.bmp")		
 		}
 	end
 
 	def test_bmp_new_bmp_3
 		2.times{
-			bmp = BMP::Reader.new("./bmp3.bmp")		
+			bmp = BMP::Reader.new("../ressources/bmp3.bmp")		
 		}
 	end
 
 	def test_bmp_matrice_1
-		bmp = BMP::Reader.new("./bmp1.bmp")
+		bmp = BMP::Reader.new("../ressources/bmp1.bmp")
 		2.times{
 			p bmp.width  #=> 2
 			p bmp.height #=> 2
 
 			0.upto(bmp.width-1) do |i|
 				0.upto(bmp.height-1) do |j|
-					assert_equal(bmp[i,j])
+					#assert_equal(bmp[i,j]) tester si =1 ou 0
 				end
 			end
 		}
