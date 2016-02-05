b8919517c32c99a3e26ad6c1539d86796da1c0d7
sakymar <minimesmes72@gmail.com>
2016-02-05 10:58:15 +0100

    Classes abstraites pour l'interface

---
+++ class/fenetreabs.class.rb
@@ -0,0 +1,52 @@
+
+
+
+
+class fenetreabs
+	def initialize(width, height, options)
+	end
+
+	def close()
+	end
+
+	def title(name)
+	end
+
+	def show(options)
+	end
+
+	def isActive?
+	end
+
+	def state
+	end
+
+	def sound
+	end
+
+	def isMuted?
+	end
+
+	def fullscreen
+	end
+
+	def isFullScreen?
+	end
+
+	def showFps
+	end
+
+	def keyUp
+	end
+
+	def keyDown
+	end
+
+	def mouseDown
+	end
+
+	#background ?
+	def draw
+	end
+
+end
---
+++ class/interfaceObject.class.rb
@@ -0,0 +1,23 @@
+class InterfaceObject
+
+	def initialize(name, option)
+	end
+
+	def parent
+	end
+
+	def destroy
+	end
+
+	def InterfaceObject.each
+	end
+
+	def InterfaceObject.all
+	end
+
+	def update
+	end
+
+
+
+end
