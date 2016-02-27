1637737852cfebf12d4174bea2ebbd5e9d3ff604
Antoine Mesnil <minimesmes72@gmail.com>
2016-02-27 21:41:33 +0100

    Correctif

--- class/text.class.rb
+++ class/text.class.rb
@@ -1,7 +1,17 @@
+#
+# Author::    Sakyamar https://github.com/sakymar
+# License::   MIT Licence
+#
+# https://github.com/Ousret/Picross-L3S6
+#
+#* Classe créant un bouton héritant de interfaceObject.
+#* Objet de l'interface du plan pouvant contenir du texte
+
+
 load './class/interfaceObject.class.rb'
 
 class Text < InterfaceObject
-	
+
 
 	#Méthode d'initialisation
 	#
@@ -10,8 +20,8 @@ class Text < InterfaceObject
 	#   - +name+ -> nom de l'objet
 	#   - +plan+ -> plan auquel l'objet est associé
 	#   - +options+ -> option supplementaire, transparence , ou autre
-	def initialize(nom,options,content)
-		
+	def initialize(nom, plan, content, options)
+
 	end
 
 	#Méthode permettant de placer l'objet dans le plan
@@ -22,12 +32,12 @@ class Text < InterfaceObject
 	#   - +height+ -> hauteur de l'objet
 	#   - +posX+ -> position horizontale à partir du coin gauche de l'objet
 	#   - +posY+ -> position verticale à partir du coin gauche de l'objet
-	#   - +content+ -> texte de l'objet	
+	#   - +content+ -> texte de l'objet
 	#   - +options+ -> option supplementaire, transparence , ou autre
 	def setup(width, height, posX, posY, content, option)
 	end
 
-	
+
 
 	#Methode permettant de changer le texte de l'objet
 	#
