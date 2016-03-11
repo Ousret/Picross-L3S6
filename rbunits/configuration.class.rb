f175ffd252a030a755fce035047bbbc03704bcf4
codeKiller227 <houssamkhalidalkassoum@gmail.com>
2016-03-11 17:42:32 +0100

    ajout de la classe configuration

---
+++ rbunits/configuration.class.rb
@@ -0,0 +1,25 @@
+# encoding: UTF-8
+
+##
+# Auteur HOUSSAM KHALID ALKASSOUM
+# Projet L3 SPI : picross
+# Version 0.1 : Date : 24/02/2016
+#
+
+require 'test/unit'
+load './class/configuration.class.rb'
+
+class TestConfiguration< Test::Unit::TestCase
+
+	#test creation d'une config de jeu
+	def test_creation()
+        matrice =[[0,0,1,0,1],[1,1,0,1,0],[0,0,1,0,0],[1,1,0,1,0],[0,1,1,1,1]]
+        profile = Profile.creer("codeKiller","AL-KASSOUM" ,"Houssam",227)
+        grille  = Grille.grille(matrice)
+		config = Configuration.configurer(grille,profile)
+		assert_equal(profile, config.profile)
+        assert_equal(grille, config.grille)
+	end
+
+end
+
