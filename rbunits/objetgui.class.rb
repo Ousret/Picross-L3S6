db818d1156124c63e64879926683407067e27ea0
TAHRI Ahmed <nyuubi.10@gmail.com>
2016-03-18 09:22:07 +0100

    Fix: Arg. Z (Plan)

--- rbunits/objetgui.class.rb
+++ rbunits/objetgui.class.rb
@@ -7,9 +7,10 @@ load './class/text.class.rb'
 
 class TestObjetGUI < Test::Unit::TestCase
 	def test_container
-		kWindow = Fenetre.creer("Picross L3-SPI", 0, 0, 800, 600)
-		kWindow.ajouterComposant(Button.creer("Partie rapide", 100, 50, 150, 200))
-		kWindow.ajouterComposant(Button.creer("Aventure", 200, 50, 150, 200))
-		kWindow.ajouterComposant(Text.creer("Welcome-Message", "Bienvenue dans le jeu Picross L3-SPI", 12, 400, 10, 0))
+    #Sur un même plan Z=0
+		kWindow = Fenetre.creer("Picross L3-SPI", 0, 0, 0, 800, 600)
+		kWindow.ajouterComposant(Button.creer("Partie rapide", 100, 50, 0, 150, 200))
+		kWindow.ajouterComposant(Button.creer("Aventure", 200, 50, 0, 150, 200))
+		kWindow.ajouterComposant(Text.creer("Welcome-Message", "Bienvenue dans le jeu Picross L3-SPI", 12, 400, 0, 10, 0))
 	end
 end
