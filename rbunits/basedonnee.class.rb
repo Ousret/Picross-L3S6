dc5829d99479ef9a904d38f08bd6799ec80c1d28
TAHRI Ahmed <nyuubi.10@gmail.com>
2016-03-24 16:52:36 +0100

    Corrections mineures dans l'écriture des tests bd

--- rbunits/basedonnee.class.rb
+++ rbunits/basedonnee.class.rb
@@ -15,7 +15,7 @@ class Testbasedonnee < Test::Unit::TestCase
 
 	def test_bdd_creation
 		kBase = Basedonnee.creer('test.db')
-		assert_equal(True, File.exist?('test.db'))
+		assert_equal(true, File.exist?('test.db'))
 	end
 
 	def test_bdd_key
@@ -26,7 +26,7 @@ class Testbasedonnee < Test::Unit::TestCase
 
 		5.times {
 			assert(kBase.addParam('test'+i.to_s, 3))
-			assert_equal(3, kBase.getParam('test'+i.to_s))
+			assert_equal(3, kBase.getValue('test'+i.to_s))
 			i += 1
 		}
 
