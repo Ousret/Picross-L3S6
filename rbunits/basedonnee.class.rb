9fd7c3b81ac938a3460d67893d7a81c5931b4800
TAHRI Ahmed <nyuubi.10@gmail.com>
2016-03-24 16:45:55 +0100

    Adaptation des tests pour la classe BaseDonnees

--- rbunits/basedonnee.class.rb
+++ rbunits/basedonnee.class.rb
@@ -14,59 +14,22 @@ load './class/basedonnee.class.rb'
 class Testbasedonnee < Test::Unit::TestCase
 
 	def test_bdd_creation
-		5.times{
-			kBase = Basedonnee.creer('test.db')
-		}
+		kBase = Basedonnee.creer('test.db')
+		assert_equal(True, File.exist?('test.db'))
 	end
 
-	def test_bdd_blob
+	def test_bdd_key
+
 		i = 0
+
 		kBase = Basedonnee.creer('test.db')
+
 		5.times {
-			assert(kBase.ajouteParamBlob('test'+i.to_s,3))
-			assert_equal(3,kBase.lireParamBlob('test'+i.to_s))
+			assert(kBase.addParam('test'+i.to_s, 3))
+			assert_equal(3, kBase.getParam('test'+i.to_s))
 			i += 1
 		}
-	end
 
-	def test_bdd_int
-		i = 0
-		kBase = Basedonnee.creer('test.db')
-		5.times{
-			assert(kBase.ajouteParamInt('test'+i.to_s,5))
-			assert_equal(5,kBase.lireParamInt('test'+i.to_s))
-			i +=1
-		}
-	end
-
-	def test_bdd_float
-		i = 0
-		kBase = Basedonnee.creer('test.db')
-		5.times{
-			assert(kBase.ajouteParamFloat('test'+i.to_s,3.7))
-			assert_equal(3.7,kBase.lireParamFloat('test'+i.to_s))
-			i += 1
-		}
-	end
-
-	def test_bdd_string
-		i = 0
-		kBase = Basedonnee.creer('test.db')
-		5.times{
-			assert(kBase.ajouteParamString('test'+i.to_s,'efgh'))
-			assert_equal('efgh',kBase.lireParamString('test'+i.to_s))
-			i += 1
-		}
-	end
-
-	def test_bdd_meme_valeur_string
-		i = 0
-		kBase = Basedonnee.creer('test.db')
-		5.times{
-			assert(kBase.ajouteParamString('test','efgh'+i.to_s))
-			assert_equal('efgh'+i.to_s,kBase.lireParamString('test'))
-			i += 1
-		}
 	end
 
 end
