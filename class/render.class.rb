1acf2179e7d31dc09c75d81ef2d70fd6be59fb85
TAHRI Ahmed <nyuubi.10@gmail.com>
2016-03-29 17:01:48 +0200

    Classe de rendu en fonction des classes TDA.

--- class/render.class.rb
+++ class/render.class.rb
@@ -4,32 +4,62 @@
 # Université du Maine
 
 require 'ray'
+require 'tmx'
 
-module Rendu
-  class Scene < Ray::Scene
+load './class/objetgui.class.rb'
+load './class/fenetre.class.rb'
+load './class/button.class.rb'
+load './class/saisie.class.rb'
+load './class/text.class.rb'
 
-    scene_name :primary
+class Rendu
 
-    def newText(unChampText)
-      text unChampText.contenu, :at => [unChampText.posx, unChampText.posy], :size => unChampText.police
-    end
+  # La fenêtre à rendre
+  attr_writer :contexte
+  attr_reader :map
 
+  def initialize(unContexteInitial)
+    @contexte = unContexteInitial
   end
 
-  class Jeu < Ray::Game
+  # Effectue le rendu en boucle si un élement bloquant si trouve (saisie & boutton)
+  # Retourne l'objet selectionné
+  def rendu()
+    #@contexte.listeComposant.each_char { |element|  }
 
-    # La fenêtre à rendre
-    attr_writer :contexte
+    Ray.game @contexte.designation do
+      register { add_hook :quit, method(:exit!) }
 
-    def initialize(unContexteInitial)
-      @contexte = unContexteInitial
+      scene :stdout do
+
+        @contexte.listeComposant.each do |composant|
+          puts "Initialisation du composant #{composant.designation}"
+          if (composant.instance_of? Text)
+            @text = text composant.contenu, :at => [composant.posx, composant.posy], :size => composant.police
+          end
+        end
+
+        always do
+          
+        end
+
+        render do |win|
+          win.draw @text
+        end
+
+      end
+      scenes << :stdout
     end
-
-    # Effectue le rendu en boucle si un élement bloquant si trouve (saisie & boutton)
-    # Retourne l'objet selectionné
-    def rendu()
-      @contexte.listeComposant.each_char { |element|  }
-    end
-
   end
+
 end
+
+
+# Tests
+kWindow = Fenetre.creer("Picross L3-SPI", 0, 0, 0, 800, 600)
+#kWindow.ajouterComposant(Button.creer("Partie rapide", 100, 50, 0, 150, 200))
+#kWindow.ajouterComposant(Button.creer("Aventure", 200, 50, 0, 150, 200))
+kWindow.ajouterComposant(Text.creer("Welcome-Message", "Bienvenue dans le jeu Picross L3-SPI", 12, 400, 50, 0))
+
+kRender = Rendu.new kWindow
+kRender.rendu
