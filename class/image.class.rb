382fc249b6c71315c6737f02373679f0730b7bc5
TAHRI Ahmed <nyuubi.10@gmail.com>
2016-04-01 10:35:30 +0200

    Méthode new TDA Image privée

--- class/image.class.rb
+++ class/image.class.rb
@@ -9,10 +9,15 @@
 class Image < ObjetGUI
 
   attr_accessor :path
+  private_class_method :new
 
   def initialize(uneDesignation, unCheminRelatif, unePositionX, unePositionY, unePositionZ)
     super(uneDesignation, unePositionX, unePositionY, unePositionZ, -1, -1)
     @path = unCheminRelatif
+  end
+
+  def Image.creer(uneDesignation, unCheminRelatif, unePositionX, unePositionY, unePositionZ)
+    new(uneDesignation, unCheminRelatif, unePositionX, unePositionY, unePositionZ)
   end
 
 end
