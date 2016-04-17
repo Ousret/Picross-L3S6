cfd89883c07a5b6e3ee05471e6c2bc4b2aa81ae6
TAHRI Ahmed <nyuubi.10@gmail.com>
2016-04-17 15:56:26 +0200

    Ajout fichier programme principal

---
+++ main.rb
@@ -0,0 +1,14 @@
+# Picross main program
+# Version 0.1.1
+
+load './class/bmp.class.rb'
+load './class/crypt.class.rb'
+load './class/registre.class.rb'
+
+load './class/objetgui.class.rb'
+load './class/fenetre.class.rb'
+load './class/button.class.rb'
+load './class/saisie.class.rb'
+load './class/text.class.rb'
+
+load './class/render.class.rb'
