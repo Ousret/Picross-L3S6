ead73c26509d7280d43f8fdc4a869ab932409898
TAHRI Ahmed <nyuubi.10@gmail.com>
2016-04-21 10:46:07 +0200

    Support du rendu boutton et interactions

--- class/render.class.rb
+++ class/render.class.rb
@@ -98,11 +98,29 @@ module Render
 
         elsif (composant.instance_of? Boutton)
           #Charge l'image boutton
-          @image = Ray::Sprite.new "ressources/images/GUI/button_base_clicked_d1l1.png"
-          @image.origin = @image.image.size / 2
-          @image.pos = [composant.posx, composant.posy]
-          @text = text composant.designation, :at => [composant.posx+20, composant.posy+20], :size => 12
-          @@vertex.push @image
+          @button = Ray::Sprite.new "ressources/images/GUI/btn_spr_m.png"
+          #@image.origin = @image.image.size / 2
+          @button.pos = [composant.posx, composant.posy]
+          @button.sheet_size = [2, 9]
+          #On place un texte
+          @text = text composant.designation, :at => [composant.posx+5, composant.posy+5], :size => 12
+
+          composant.taillex = @button.image.size.to_a[0]
+          composant.tailley = @button.image.size.to_a[1]
+
+          always do
+            if animations.empty?
+              if (composant.isOver(mouse_pos.to_a[0], mouse_pos.to_a[1]) && !composant.survol)
+                animations << sprite_animation(:from => [0, 1], :to => [1, 1], :duration => 0.2).start(@button)
+                composant.survol = true
+              elsif (composant.survol && !composant.isOver(mouse_pos.to_a[0], mouse_pos.to_a[1]))
+                animations << sprite_animation(:from => [1, 1], :to => [0, 1], :duration => 0.2).start(@button)
+                composant.survol = false
+              end
+            end
+          end
+          
+          @@vertex.push @button
           @@vertex.push @text
         elsif (composant.instance_of? Sprite)
           @@vertex.push createSprite composant
@@ -154,10 +172,12 @@ end
 
 # Tests
 kWindow = Fenetre.creer("Picross L3-SPI", 0, 0, 0, 800, 600)
-#kWindow.ajouterComposant(Boutton.creer("Partie rapide", 100, 50, 0, 150, 200))
+
 kWindow.ajouterComposant(Image.creer("ImageTest", "ressources/maps/OpenWorld3.png", 250, 20, 0))
 #kWindow.ajouterComposant(Boutton.creer("Aventure", 200, 50, 0, 150, 200))
 kWindow.ajouterComposant(Text.creer("Welcome-Message", "alpha-preview 1", 15, 20, 20, 0))
 kWindow.ajouterComposant(Sprite.creer("SpriteHero", "ressources/images/sprites/Characters/MrYtdBCF.png", 13, 21, 20, 20, 0, 100, 100))
+kWindow.ajouterComposant(Boutton.creer("Partie rapide", 100, 50, 0, 150, 200))
 
-Thread.new {Render::Game.new.prepare kWindow}
+kRender = Render::Game.new
+kRender.prepare kWindow
