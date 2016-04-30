e0d973365b53f85a4f98ebe6de68e90de09bd05d
TAHRI Ahmed <nyuubi.10@gmail.com>
2016-04-30 18:59:12 +0200

    Fix ident sur classe Chrono

--- class/chrono.class.rb
+++ class/chrono.class.rb
@@ -10,52 +10,52 @@
 class  Chrono
 
 	# la Classe Chrono permet de gerer un chrono (creation/start/stop)
-	
+
 	#=== Variables d'instance ===
 	#@start_time	#date de debut
-    #@total_time  	#temps ecoulé depuis le debut
-    #@total_time_acc #accumulateur du temp total
-    #@nbHeure		#nombre d'heures ecoulé
-    #@nbMinute		#nombre de minutes ecoulé
-    #@nbSeconde		#nombre de secondes ecoulé
-    #@thread        #thread pour le calcule du temp
-    #@enPause  		#boolean qui permet de savoir si le chono est en pause
+	#@total_time  	#temps ecoulé depuis le debut
+	#@total_time_acc #accumulateur du temp total
+	#@nbHeure		#nombre d'heures ecoulé
+	#@nbMinute		#nombre de minutes ecoulé
+	#@nbSeconde		#nombre de secondes ecoulé
+	#@thread        #thread pour le calcule du temp
+	#@enPause  		#boolean qui permet de savoir si le chono est en pause
 	#============================
-
+	
 	#la methode new() est private pour cette classe
-	private_class_method :new 
+	private_class_method :new
 
-    #Définition des methodes d'accèes en lecture
+	#Définition des methodes d'accèes en lecture
 	attr_reader :total_time , :nbHeure , :nbMinute , :nbSeconde , :total_time_acc
 
 
 	def initialize(t)#:nodoc:
-	
+
 		@total_time	= 0
-		@nbHeure	= 0	
-    	@nbMinute	= 0	
-    	@nbSeconde	= 0
-    	@total_time_acc = t
+		@nbHeure	= 0
+		@nbMinute	= 0
+		@nbSeconde	= 0
+		@total_time_acc = t
 		@enPause	= true
 
-		@thr = Thread.new { 
+		@thr = Thread.new {
 
-				while true do
+			while true do
 
-					if(!@enPause)
-			   			@total_time = Time.now - @start_time
-				   		@nbSeconde = (@total_time + @total_time_acc).to_i
-				   		@nbHeure   = @nbSeconde / 3600
-			    		@nbMinute  = (@nbSeconde - 3600*@nbHeure)/60
-			    		@nbSeconde = (@nbSeconde - 3600*@nbHeure -@nbMinute*60)
-			    	end
-
+				if(!@enPause)
+					@total_time = Time.now - @start_time
+					@nbSeconde = (@total_time + @total_time_acc).to_i
+					@nbHeure   = @nbSeconde / 3600
+					@nbMinute  = (@nbSeconde - 3600*@nbHeure)/60
+					@nbSeconde = (@nbSeconde - 3600*@nbHeure -@nbMinute*60)
 				end
-		 	}
+
+			end
+		}
 
 	end
 
-	
+
 	#=== Methode de classe permetant la creation d'un chrono
 	#
 	#=== Paramètres:
@@ -82,43 +82,43 @@ class  Chrono
 		@start_time = Time.now
 		@total_time_acc = @total_time_acc + @total_time
 		@enPause	= false
-    end
+	end
 
-    #=== Methode qui permet de stoper le chrono
+	#=== Methode qui permet de stoper le chrono
 	#
 	#=== Paramètres:
 	#* <b>pas de paramètre</b>
 	def arreter()
 		@enPause	= true
-    end
+	end
 
-    #=== Methode qui permet de recuperer le temp totale 
+	#=== Methode qui permet de recuperer le temp totale
 	#
 	#=== Paramètres:
 	#* <b>pas de paramètre</b>
 	def getTTotale()
 		return (@total_time + @total_time_acc).to_i
-    end
+	end
 
-    #=== Methode qui permet de redemarer le chrono
+	#=== Methode qui permet de redemarer le chrono
 	#=== Paramètres:
 	#* <b>pas de paramètre</b>
-    def reset()
+	def reset()
 
-    	@total_time	= 0
-		@nbHeure	= 0	
-    	@nbMinute	= 0	
-    	@nbSeconde	= 0
-    	@total_time_acc = 0
-    end
+		@total_time	= 0
+		@nbHeure	= 0
+		@nbMinute	= 0
+		@nbSeconde	= 0
+		@total_time_acc = 0
+	end
 
-    #=== Methode qui permet de stoper le chrono
+	#=== Methode qui permet de stoper le chrono
 	#
 	#=== Paramètres:
 	#* <b>pas de paramètre</b>
 	def to_s()
 
 		return (@nbHeure.to_s+" : "+@nbMinute.to_s+" : "+@nbSeconde.to_s)
-    end
+	end
 
 end
