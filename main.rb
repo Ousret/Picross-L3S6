d13088a6abdddf3ad5631b4dd2d27d1a5501c448
TAHRI Ahmed <nyuubi.10@gmail.com>
2016-05-01 18:19:59 +0200

    Fenêtre À propos

--- main.rb
+++ main.rb
@@ -25,6 +25,7 @@ class Jeu
   ADVENTURE = "Aventure"
   QUIT = "Quitter"
   BACK = "Retour"
+  URL_FETCH = "http://hop3x.univ-lemans.fr/picross/levels.json"
 
   def initialize()
 
@@ -36,6 +37,7 @@ class Jeu
 
     @kMainMenu = Fenetre.creer("Menu principal", 0, 0, 0, 640, 480)
     @kInGame = Fenetre.creer("Jeu", 0, 0, 0, 640, 480)
+    @kAbout = Fenetre.creer("À propos", 0, 0, 0, 640, 480)
 
     # Récupère les statistiques du disque local
     getStats
@@ -101,22 +103,61 @@ class Jeu
     @kRegistre.addParam("lastLevel", "1")
   end
 
+  def addStatBox(unContexteCible)
+
+    stat_support = Image.creer("Stat-background", "ressources/images/GUI/box/boxNormal.png", 480, 20, 1)
+
+    libell_coins = Text.creer("coins", "Gems   : #{@coins||0}", 10, 500, 50, 2)
+    libell_try = Text.creer("try", "Essai    : #{@nTry||0}", 10, 500, 60, 2)
+    libell_win = Text.creer("win", "Victoire: #{@nWin||0}", 10, 500, 70, 2)
+    libell_avancement = Text.creer("avancement", "Niveau: #{@lastLevel||0}/#{@nLevel||0}", 10, 500, 80, 2)
+
+    unContexteCible.ajouterComposant(stat_support, libell_coins, libell_try, libell_win, libell_avancement)
+
+  end
+
+  def initializeAbout
+
+    @kAbout.supprimeTout
+    # Image de fond
+    background = Image.creer("Background", "ressources/images/GUI/Prototypes/background-6.png", 0, 0, 0)
+    # Information sur version
+    libell_alpha = Text.creer("beta-1", "beta-preview 1", 15, 100, 120, 1)
+    # Placement du titre
+    libell_title = Text.creer("title", "Picross B", 50, 100, 100, 1)
+    libell_title.setPolice "ressources/ttf/Starjedi.ttf" #Police d'écriture spéciale pour le titre
+
+    libell_line1 = Text.creer("about_1", "Jeu de Picross Grpe B", 12, 100, 200, 1)
+    libell_line2 = Text.creer("about_2", "Projet de semestre 6 en L3 SPI", 12, 100, 215, 1)
+
+    libell_line3 = Text.creer("about_3", "Moteur Jeu: Houssam",12, 100, 230, 1)
+    libell_line4 = Text.creer("about_4", "RenduGL: Ahmed", 12, 100, 245, 1)
+    libell_line5 = Text.creer("about_5", "Registre: Victorien, Ahmed",12, 100, 260, 1)
+    libell_line6 = Text.creer("about_6", "Cryptage: Baptiste", 12,100, 275, 1)
+    libell_line7 = Text.creer("about_7", "TDA GUI: Antoine, Ahmed",12, 100, 290, 1)
+    libell_line8 = Text.creer("about_8", "BMP: Marius",12, 100, 305, 1)
+    libell_line9 = Text.creer("about_9", "Recherches: Matthis",12, 100, 320, 1)
+
+    btn_quit = Boutton.creer("Retour", 50, 400, 1, 0, 0)
+
+    addStatBox @kAbout
+
+    @kAbout.ajouterComposant background, libell_alpha, libell_title, libell_line1, libell_line2, libell_line3, libell_line4, libell_line5, libell_line6, libell_line7, libell_line8, libell_line9, btn_quit
+
+  end
+
   def initializeMainMenu()
 
     @kMainMenu.supprimeTout
 
     # Image de fond
     background = Image.creer("Background", "ressources/images/GUI/Prototypes/background-5.png", 0, 0, 0)
-    stat_support = Image.creer("Stat-background", "ressources/images/GUI/box/boxNormal.png", 480, 20, 1)
 
     # Information sur version
     libell_alpha = Text.creer("beta-1", "beta-preview 1", 15, 250, 170, 1)
 
     # Statistique
-    libell_coins = Text.creer("coins", "Gems   : #{@coins||0}", 10, 500, 50, 1)
-    libell_try = Text.creer("try", "Essai    : #{@nTry||0}", 10, 500, 60, 1)
-    libell_win = Text.creer("win", "Victoire: #{@nWin||0}", 10, 500, 70, 1)
-    libell_avancement = Text.creer("avancement", "Niveau: #{@lastLevel||0}/#{@nLevel||0}", 10, 500, 80, 1)
+    addStatBox @kMainMenu
 
     # Placement du titre
     libell_title = Text.creer("title", "Picross B", 50, 250, 150, 1)
@@ -127,21 +168,23 @@ class Jeu
     btn_newGame = Boutton.creer("Partie rapide", 40, 100, 1, 0, 0)
     btn_params = Boutton.creer("Parametres", 40, 150, 1, 0, 0)
     btn_about = Boutton.creer("À propos", 40, 200, 1, 0, 0)
-    btn_quit = Boutton.creer("Quitter", 40, 250, 0, 1, 0)
+    btn_quit = Boutton.creer("Quitter", 40, 250, 1, 0, 0)
 
     # Ajout des composants sur la fenêtre primaire
-    @kMainMenu.ajouterComposant(background, stat_support, libell_alpha, libell_coins ,libell_title,libell_try, libell_win, libell_avancement ,btn_aventure, btn_newGame, btn_params,btn_about ,btn_quit)
+    @kMainMenu.ajouterComposant(background, libell_alpha, libell_title ,btn_aventure, btn_newGame, btn_params,btn_about ,btn_quit)
 
   end
 
-  def initializeGame()
+  def initializeGame
     @kInGame.supprimeTout
 
     background = Image.creer("Background", "ressources/maps/Couloirs-Resized.png", 0, 0, 0)
-    libell_score = Text.creer("score", "Gems: 0", 12, 50, 70, 1)
 
     libell_niveau = Text.creer("niveau", "Niveau #{@lastLevel}", 19, 50, 40, 1)
     libell_niveau.setPolice "ressources/ttf/Starjedi.ttf"
+
+    # Statistique
+    addStatBox @kInGame
 
     myMat = getMatrice
     support_grille = Image.creer("Grille", "ressources/images/Grilles/v3/g#{myMat.length}x#{myMat.length}.png", 120, 120, 1)
@@ -149,10 +192,10 @@ class Jeu
     @currentGame = Grille.grille(myMat)
     addTry # Ajout +1 aux essai (statistiques)
 
-    btn_quit = Boutton.creer("Retour", 50, 430, 1, 0, 0)
+    btn_quit = Boutton.creer("Abandonner", 50, 430, 1, 0, 0)
     btn_help = Boutton.creer("Aide", 150, 430, 1, 0, 0)
 
-    @kInGame.ajouterComposant(background, libell_score, libell_niveau, support_grille)
+    @kInGame.ajouterComposant(background, libell_niveau, support_grille)
 
     # On place des élements "case" pour grille
     inHautPosX = 244
@@ -224,6 +267,8 @@ class Jeu
       exit
     elsif (btn_cible_libell == "À propos")
       #On charge la fenêtre à propos
+      initializeAbout
+      @kRender.end_scene @kAbout
     end
   end
 
@@ -232,7 +277,7 @@ class Jeu
     return if unTypeEvenement != 1 # On ne recherche que le clique souris
     btn_cible_libell = unComposantCible.designation
 
-    if (btn_cible_libell == "Retour") #Si le joueur clique sur "Retour" depuis la séance de jeu
+    if (btn_cible_libell == "Abandonner") #Si le joueur clique sur "Retour" depuis la séance de jeu
       #On charge le menu principal
       initializeMainMenu
       #On recharge le rendu
@@ -242,6 +287,9 @@ class Jeu
       # On tente de noirsir la case
       if @currentGame.noirsirCase unComposantCible.arr_data[0], unComposantCible.arr_data[1]
         @kRender.game_scenes.animateSprite unComposantCible
+        @kRender.game_scenes.instantSound "ressources/son/pop.wav"
+      else
+        @kRender.game_scenes.instantSound "ressources/son/beep.wav"
       end
       # On vérifie que l'état de la partie
       if @currentGame.terminer?
@@ -251,7 +299,15 @@ class Jeu
         initializeMainMenu
         @kRender.end_scene @kMainMenu
       end
-
+    elsif (btn_cible_libell == "Aide")
+      #On vérifie que le joueur a suffisament de credit
+      if (@coins > 100*@lastLevel)
+        caseCible = @currentGame.demanderAide
+        if (caseCible != [-1, -1])
+          @currentGame.noirsirCase caseCible[0], caseCible[1]
+          @kRender.game_scenes.instantSound "ressources/son/pop.wav"
+        end
+      end
     elsif (btn_cible_libell == "Quitter")
       #On met fin au programme
       exit
@@ -259,15 +315,29 @@ class Jeu
 
   end
 
+  def actionOnAbout(unTypeEvenement, unComposantCible)
+    # Gestion des événements sur menu principal
+    return if unTypeEvenement != 1 # On ne recherche que le clique souris
+    btn_cible_libell = unComposantCible.designation
+
+    if (btn_cible_libell == "Retour")
+      initializeMainMenu
+      @kRender.end_scene @kMainMenu
+    end
+    
+  end
+
   # Méthode de reception signal pour pattern observateur
   def update(unTypeEvenement, unComposantCible)
-    puts "Attention: Evenement Trigger #{unComposantCible.designation} sur typeEvenement = #{unTypeEvenement} avec contexte = #{@kRender.getContext.designation}"
+    #puts "Attention: Evenement Trigger #{unComposantCible.designation} sur typeEvenement = #{unTypeEvenement} avec contexte = #{@kRender.getContext.designation}"
 
     #On redirige vers la méthode concernée
     if @kRender.getContext.designation == "Menu principal"
       actionOnMenu unTypeEvenement, unComposantCible
     elsif @kRender.getContext.designation == "Jeu"
       actionOnGame unTypeEvenement, unComposantCible
+    elsif @kRender.getContext.designation == "À propos"
+      actionOnAbout unTypeEvenement, unComposantCible
     end
 
   end
